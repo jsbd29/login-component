773f5bad846fa137912a610544154af5d1e942fb
Sebastian Dutra <30559771+jsbd29@users.noreply.github.com>
2026-02-27 09:37:51 -0600

    Implement initial layout for App component
    Add basic structure for App component with sidebar and body sections.

--- App.jsx
+++ App.jsx
@@ -1,1 +1,27 @@
 
+
+
+import './App.css'
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className='App'>
+        <div className='sidebar_container'> I am the side bar</div>
+        <div className='body_container'>
+          <hr/> I am body
+          <hr/> I am body
+          <hr/> I am body
+          <hr/> I am body
+          <hr/> I am body
+          <hr/> I am body
+       </div>
+      </div>
+    </>
+  )
+}
+
+export default App
